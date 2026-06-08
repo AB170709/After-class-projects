39f5810bfaac053d67125e7460bd11e05f0fbe80
AB170709 <itachibakshi@gmail.com>
2026-06-08 15:31:13 +0530

    Add files via upload

---
+++ swapping_hw.py
@@ -0,0 +1,12 @@
+x=int(input("enter value of x:"))
+y=int(input("enter value of y:"))
+z=int(input("enter value of z:"))
+
+temp=x
+x=y
+y=z
+z=temp
+
+print("value of x after swapping:",x)
+print("value of y after swapping:",y)
+print("value of y after swapping:",z)
